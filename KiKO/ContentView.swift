6fbc8f002fa783a4d6fc5f5d342cd800c32d639b
Joscha Amani Gaber <joscha.amani.gaber@gmail.com>
2024-11-04 13:54:45 +0100

    Initial Commit

---
+++ KiKO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KiKO
+//
+//  Created by Joscha Amani Gaber on 04.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KiKO/KiKOApp.swift
@@ -0,0 +1,17 @@
+//
+//  KiKOApp.swift
+//  KiKO
+//
+//  Created by Joscha Amani Gaber on 04.11.24.
+//
+
+import SwiftUI
+
+@main
+struct KiKOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
